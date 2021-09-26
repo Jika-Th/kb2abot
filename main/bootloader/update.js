c98218a26870165c747c9f3468f98af4a0885afc
Nguyễn Thanh Chính <73381800+Citnut@users.noreply.github.com>
2021-09-26 10:53:15 +0700

    Update update.js

--- main/bootloader/update.js
+++ main/bootloader/update.js
@@ -5,11 +5,11 @@ module.exports = {
 	des: 'Kiem tra va cap nhat kb2abot',
 	fn: async () => {
 		const {data} = await axios.get(
-			'https://raw.githubusercontent.com/kb2ateam/kb2abot/main/package.json'
+			'https://raw.githubusercontent.com/Citnut/kb2abotpro/main/package.json'
 		);
 		if (data.version != version) {
 			const {data: info} = await axios.get(
-				'https://api.github.com/repos/kb2ateam/kb2abot/git/refs/heads/main'
+				'https://api.github.com/repos/Citnut/kb2abotpro/git/refs/heads/main'
 			);
 			const {data: commit} = await axios.get(info.object.url);
 			console.log();
