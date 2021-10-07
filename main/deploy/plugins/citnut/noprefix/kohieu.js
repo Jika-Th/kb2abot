3cb5efed271968ab8ec1bbb83a096c02fcce6620
Nguyễn Thanh Chính <73381800+Citnut@users.noreply.github.com>
2021-10-07 21:47:02 +0700

    citnut 1.5.2 fix

--- main/deploy/plugins/citnut/noprefix/kohieu.js
+++ main/deploy/plugins/citnut/noprefix/kohieu.js
@@ -1,5 +1,5 @@
 const { getFile } = kb2abot.helpers;
-let attachment = getFile("./main/deploy/plugins/citnut/data/noprefix/NHD.jpg");
+let attachment = getFile("./main/deploy/plugins/citnut/data/quynhhh.mp3");
 
 module.exports = {
 	keywords: ["kohieu", "khonghieu"],
@@ -34,7 +34,7 @@ module.exports = {
 	async onMessage(message, reply) {
 		let setting = this.storage.account.global.citSetting;
 		if (setting.autorun != true || message.senderID == this.storage.account.global.console.bot.id) {
-		}else if (message.body.toLowerCase().inculdes("ko hiểu") || message.body.toLowerCase().inculdes("không hiểu")) {
+		}else if (message.body.toLowerCase().includes("ko hiểu") || message.body.toLowerCase().includes("không hiểu")) {
 			if (message.type == "message_reply" && message.messageReply.body == "bạn không hiểu thật hỏ...") return;
 			reply({body: "bạn không hiểu thật hỏ...",attachment})
 		}
